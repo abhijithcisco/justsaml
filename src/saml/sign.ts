bed6e925de64e9a92828ff1e34ee56e43a9d2205
msabhijith <msabhijith@gmail.com>
2026-02-11 12:52:20 +0530

    Add dual signature support for SAML Response and Assertion elements

--- src/saml/sign.ts
+++ src/saml/sign.ts
@@ -5,15 +5,17 @@ export function signSamlResponse(xml: string, privateKeyPath: string, certPath: 
   const privateKey = fs.readFileSync(privateKeyPath, "utf-8");
   const publicCert = fs.readFileSync(certPath, "utf-8");
 
-  const sig = new SignedXml({
+  const sigOpts = {
     privateKey,
     publicCert,
     canonicalizationAlgorithm: "http://www.w3.org/2001/10/xml-exc-c14n#",
     signatureAlgorithm: "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
     getKeyInfoContent: SignedXml.getKeyInfoContent,
-  });
+  };
 
-  sig.addReference({
+  // Step 1: Sign the Assertion
+  const assertionSig = new SignedXml(sigOpts);
+  assertionSig.addReference({
     xpath: "//*[local-name(.)='Assertion']",
     transforms: [
       "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
@@ -21,13 +23,30 @@ export function signSamlResponse(xml: string, privateKeyPath: string, certPath: 
     ],
     digestAlgorithm: "http://www.w3.org/2001/04/xmlenc#sha256",
   });
-
-  sig.computeSignature(xml, {
+  assertionSig.computeSignature(xml, {
     location: {
-      reference: "//*[local-name(.)='Issuer']",
+      reference: "//*[local-name(.)='Assertion']/*[local-name(.)='Issuer']",
+      action: "after",
+    },
+  });
+  const assertionSignedXml = assertionSig.getSignedXml();
+
+  // Step 2: Sign the Response
+  const responseSig = new SignedXml(sigOpts);
+  responseSig.addReference({
+    xpath: "//*[local-name(.)='Response']",
+    transforms: [
+      "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
+      "http://www.w3.org/2001/10/xml-exc-c14n#",
+    ],
+    digestAlgorithm: "http://www.w3.org/2001/04/xmlenc#sha256",
+  });
+  responseSig.computeSignature(assertionSignedXml, {
+    location: {
+      reference: "//*[local-name(.)='Response']/*[local-name(.)='Issuer']",
       action: "after",
     },
   });
 
-  return sig.getSignedXml();
+  return responseSig.getSignedXml();
 }
